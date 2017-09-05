a28431d6dbaf2c01b2a22d161c1aab3e73a6a22a
Panda <linimbus@126.com>
2017-09-05 21:03:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 // 服务端demo
 func Server() {
-	ntps := ntp.NewNTPS("", "1234")
+	ntps := ntp.NewNTPS("dev0", "3210")
 	ntps.Start()
 	for {
 		time.Sleep(1 * time.Second)
@@ -19,23 +19,57 @@ func Server() {
 
 // 客户端demo
 func Client() {
-	ntpc := ntp.NewNTPC("192.168.0.101", "1234")
 
-	for i := 0; i < 1000; i++ {
+	ntpc := ntp.NewNTPC("dev0", "3210")
+
+	for {
 		time.Sleep(1 * time.Second)
 
-		result, err := ntpc.Sync(10)
-		if err != nil {
-			fmt.Println(err.Error())
-			break
+		resultAry := make([]ntp.Result, 10)
+
+		for i, _ := range resultAry {
+			rsp, err := ntpc.Sync(1)
+			if err != nil {
+				fmt.Println(err.Error())
+				continue
+			}
+			resultAry[i] = rsp
 		}
+
+		var result ntp.Result
+		var count int64
+		for _, v := range resultAry {
+			if v.NetDelay.NanoSecond > 0 {
+				result.NetDelay.Add(v.NetDelay)
+				result.Offset.Add(v.Offset)
+				count++
+			}
+		}
+
+		result.NetDelay.Div(count)
+		result.Offset.Div(count)
 
 		fmt.Printf("offset %.3f ms \r\n", float64(result.Offset.NanoSecond)/float64(time.Millisecond))
 		fmt.Printf("netdelay %.3f ms \r\n", float64(result.NetDelay.NanoSecond)/float64(time.Millisecond))
 
 		if result.Offset.Abs() > int64(time.Second) {
 			now := ntp.TimeStampToTime(result.Offset, time.Now())
-			err = ntp.SetTimeToOs(now)
+			fmt.Println(result.Offset)
+
+			err := ntp.SetTimeToOs(now)
+			if err != nil {
+				fmt.Println(err.Error())
+				break
+			}
+		} else if result.Offset.Abs() > 100*int64(time.Millisecond) {
+			mul := result.Offset.Abs() / (100 * int64(time.Millisecond))
+
+			var offset ntp.TimeStamp
+			offset.NanoSecond = 25 * mul * result.Offset.AbsValue() * int64(time.Millisecond)
+			now := ntp.TimeStampToTime(offset, time.Now())
+			fmt.Println(result.Offset, offset)
+
+			err := ntp.SetTimeToOs(now)
 			if err != nil {
 				fmt.Println(err.Error())
 				break
